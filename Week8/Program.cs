3840bdca22ccee5d3e907c4b7184b83355333e01
yelyzaveta <yelyzaveta.voitovych.25@kse.org.ua>
2026-03-28 17:47:28 +0200

    3 stage is finished

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,26 @@
 ﻿
+using Week8;
+
 class Program
 {
     public static void Main()
     {
+        var container = new DataContainer<DataItem>();
+        for (int i = 0; i < 10000; i++)
+        {
+            var item = new DataItem
+            {
+                Id = $"item-{i}",
+                Payload = new byte[1024]
+            };
+            container.AddItem(item);
+        }
+        Console.WriteLine($"Total size before GC: {container.GetTotalSize()} bytes");
+
+        GC.Collect();
         
+        Console.WriteLine("GC.Collect() was called.");
+        Console.WriteLine($"Total size after GC: {container.GetTotalSize()} bytes"); 
     }
     
 }
