1d36b8f34d47fe4381d26668002865b750058824
yelyzaveta <yelyzaveta.voitovych.25@kse.org.ua>
2026-03-28 17:18:19 +0200

    1 stage is finished

---
+++ Week8/Point.cs
@@ -0,0 +1,11 @@
+namespace Week8;
+
+public struct Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public void Print()
+    {
+        Console.WriteLine($"Coordinates - ({X}, {Y})");
+    }
+}
