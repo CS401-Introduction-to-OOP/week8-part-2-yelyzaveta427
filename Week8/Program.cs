72694903893c3f72d2ffcd5b1e4197fd47b8cf11
yelyzaveta <yelyzaveta.voitovych.25@kse.org.ua>
2026-03-28 17:06:57 +0200

    3 stage is finished

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,9 +1,24 @@
 ﻿
+using Week8;
+
 class Program
 {
     public static void Main()
     {
+        var fileRes = new FileResource("Document");
+        var netRes = new NetworkResource("123.234.123.12");
+
+        var manager = new ResourceManager<Resource>();
+        manager.Add(fileRes);
+        manager.Add(netRes);
         
+        manager.OpenAll();
+        using(var res = new FileResource("math"))
+        {
+            res.Open();
+        }
+        
+        manager.CloseAll();
     }
     
 }
